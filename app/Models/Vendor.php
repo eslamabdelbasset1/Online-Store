bc2fa8505581b484c9a8ae66e58f6689c04760b7
eslam <eslamelbazedy@gmail.com>
2021-11-06 18:22:14 +0200

    init commit

---
+++ app/Models/Vendor.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vendor extends Model
+{
+    use HasFactory;
+
+    protected $table = 'vendors';
+
+    protected $fillable = [
+        'latitude', 'longitude', 'name', 'mobile', 'password', 'address', 'email', 'logo', 'category_id', 'active', 'created_at', 'updated_at'
+    ];
+}
