bca7c02027b400dca155457a7ca3047aeb653b0d
eslam <eslamelbazedy@gmail.com>
2021-11-10 22:39:38 +0200

    init commit

--- app/Http/Controllers/VendorsController.php
+++ app/Http/Controllers/VendorsController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\VendorRequest;
 use App\Models\MainCategory;
 use App\Models\Vendor;
+use App\Notifications\VendorCreated;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
 
 class VendorsController extends Controller
 {
@@ -20,9 +23,42 @@ class VendorsController extends Controller
         return view('admin.vendors.create', compact('categories'));
     }
 
-    public function store(Request $request)
+    public function store(VendorRequest $request)
     {
-        return $request;
+        try {
+
+            if (!$request->has('active'))
+                $request->request->add(['active' => 0]);
+            else
+                $request->request->add(['active' => 1]);
+
+            $filePath = "";
+            if ($request->has('logo')) {
+                $filePath = uploadImage('vendors', $request->logo);
+            }
+
+            $vendor = Vendor::create([
+                'name' => $request->name,
+                'mobile' => $request->mobile,
+                'email' => $request->email,
+                'active' => $request->active,
+                'address' => $request->address,
+                'logo' => $filePath,
+                'password' => $request->password,
+                'category_id' => $request->category_id,
+                'latitude' => $request->latitude,
+                'longitude' => $request->longitude,
+            ]);
+
+            Notification::send($vendor, new VendorCreated($vendor));
+
+            return redirect()->route('admin.vendors')->with(['success' => 'تم الحفظ بنجاح']);
+
+        } catch (\Exception $ex) {
+            return $ex;
+            return redirect()->route('admin.vendors')->with(['error' => 'حدث خطا ما برجاء المحاوله لاحقا']);
+
+        }
     }
 
 }
--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Vendor extends Model
 {
+    use Notifiable;
     use HasFactory;
 
     protected $table = 'vendors';
@@ -14,10 +16,11 @@ class Vendor extends Model
     protected $fillable = [
         'latitude', 'longitude', 'name', 'mobile', 'password', 'address', 'email', 'logo', 'category_id', 'active', 'created_at', 'updated_at'
     ];
+    protected $hidden = ['category_id', 'password'];
 
     public function getLogoAttribute($val)
     {
-        return ($val !== null) ? asset('assets/' . $val) : "";
+        return ($val !== null) ? asset('public/assets/' . $val) : "";
 
     }
 
@@ -36,6 +39,19 @@ class Vendor extends Model
     {
 
         return $this->belongsTo('App\Models\MainCategory', 'category_id', 'id');
+    }
+
+    public function getActive()
+    {
+        return $this->active == 1 ? 'مفعل' : 'غير مفعل';
+
+    }
+
+    public function setPasswordAttribute($password)
+    {
+        if (!empty($password)) {
+            $this->attributes['password'] = bcrypt($password);
+        }
     }
 
 }
